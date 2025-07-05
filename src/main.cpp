425a3bc5ad8863bc97a3a2fe4b1116a26a8125bc
kai-1208 <ra23070n@st.omu.ac.jp>
2025-07-05 11:27:36 +0900

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,13 @@
 
 int main() {
     BufferedSerial pc(USBTX, USBRX, 115200);
-    Amt212CV encoder(PB_6, PA_10, D6, 0x54);  // TX, RX, DE/RE, address
+    Amt212CV encoder(PB_6, PA_10, D6, 0x54);  // TX, RX, DE/RE, address　適当に変えてください
 
     while (true) {
         if (encoder.update()) {
             auto angle = encoder.get_angle();
             auto ticks = encoder.get_position();
-            printf("AMT212C-V ticks: %u, angle: %f [rad], %f [deg]\n",
-                   ticks, angle.rad(), angle.deg());
+            printf("AMT212C-V ticks: %u, angle: %f [rad], %f [deg]\n", ticks, angle.rad(), angle.deg());
         } else {
             printf("Failed to read encoder\n");
         }
