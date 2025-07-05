5bba18ca4de0f98893bac01e17a3020da76599d9
kai-1208 <ra23070n@st.omu.ac.jp>
2025-07-05 12:14:40 +0900

    Add support for a second Amt212CV encoder and update output formatting

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 
 int main() {
     BufferedSerial pc(USBTX, USBRX, 115200);
+    // エンコーダーの数増やすなら、アドレスを別々に決めてください。
     Amt212CV encoder(PB_6, PA_10, D6, 0x54);
+    // Amt212CV encoder2(PB_6, PA_10, D6, 0x50);
     encoder.set_mode(Amt212CV::Mode::Continuous);  // Continuousモードで∞範囲に追従、Wrappedモードで0~2π範囲に追従
+    // encoder2.set_mode(Amt212CV::Mode::Continuous);
 
     while (true) {
         if (encoder.update()) {
@@ -12,10 +15,19 @@ int main() {
             float rad = encoder.get_angle().rad();
             int32_t pos = encoder.get_position();
 
-            printf("Ticks: %ld, Angle: %.3f [deg], %.3f [rad]\n", pos, deg, rad);
+            printf("Encoder 1 - Ticks: %ld, Angle: %.3f [deg], %.3f [rad]\n", pos, deg, rad);
         } else {
             printf("Failed to read encoder\n");
         }
+        // if (encoder2.update()) {
+        //     float deg2 = encoder2.get_angle().deg();
+        //     float rad2 = encoder2.get_angle().rad();
+        //     int32_t pos2 = encoder2.get_position();
+
+        //     printf("Encoder 2 - Ticks: %ld, Angle: %.3f [deg], %.3f [rad]\n", pos2, deg2, rad2);
+        // } else {
+        //     printf("Failed to read encoder 2\n");
+        // }
         ThisThread::sleep_for(100ms);
     }
 }
