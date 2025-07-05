f78628f58642e40bf2d0b0ee6219f6a5a1998977
kai-1208 <ra23070n@st.omu.ac.jp>
2025-07-05 11:23:29 +0900

    first commit

---
+++ src/amt212c_v.hpp
@@ -0,0 +1,74 @@
+#ifndef AMT212C_V_HPP
+#define AMT212C_V_HPP
+
+#include <mbed.h>
+#include <cmath>
+
+namespace anglelib {
+
+constexpr float PI = 3.14159265358979323846f;
+constexpr float TAU = 2.0f * PI;
+
+template<typename T>
+constexpr bool is_equal_approx(T a, T b) {
+    constexpr T EPS = 1e-5f;
+    return std::abs(a - b) < EPS;
+}
+
+template<typename T>
+constexpr T wrapf(T value, T min, T max) {
+    T range = max - min;
+    if (is_equal_approx(range, T(0))) return min;
+    T result = value - range * std::floor((value - min) / range);
+    return is_equal_approx(result, max) ? min : result;
+}
+
+template<typename Rep>
+struct Angle {
+    Rep value;
+    Angle() : value(0) {}
+    explicit Angle(Rep rad) : value(rad) {}
+
+    static Angle from_deg(Rep deg) { return Angle(deg * (TAU / 360)); }
+    static Angle from_rad(Rep rad) { return Angle(rad); }
+
+    Rep rad() const { return value; }
+    Rep deg() const { return value * (360.0f / TAU); }
+
+    Angle operator+(const Angle& rhs) const { return Angle(value + rhs.value); }
+    Angle operator-(const Angle& rhs) const { return Angle(value - rhs.value); }
+    Angle operator*(Rep rhs) const { return Angle(value * rhs); }
+    Angle abs() const { return Angle(std::abs(value)); }
+};
+
+using Anglef = Angle<float>;
+
+} // namespace anglelib
+
+class Amt212CV {
+public:
+    Amt212CV(PinName tx, PinName rx, PinName dere, uint8_t address, int baud = 2000000);
+    bool update();
+    anglelib::Anglef get_angle() const;
+    uint16_t get_position() const;
+
+private:
+    UnbufferedSerial rs485;
+    DigitalOut de;
+    Timer timer;
+    uint8_t addr;
+    float scale = 1.0f;
+    anglelib::Anglef offset = anglelib::Anglef::from_rad(0.0f);
+    anglelib::Anglef angle{};
+    anglelib::Anglef last_angle{};
+    uint16_t ticks = 0;
+    bool initialized = false;
+
+    void flush();
+    void send(const void* data, size_t len);
+    bool recv(void* data, size_t len, std::chrono::microseconds timeout);
+    bool read_angle(anglelib::Anglef& result);
+    static bool is_valid(uint16_t data);
+};
+
+#endif // AMT212C_V_HPP
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <mbed.h>
+#include "amt212c_v.hpp"
+
+int main() {
+    BufferedSerial pc(USBTX, USBRX, 115200);
+    Amt212CV encoder(PB_6, PA_10, D6, 0x54);  // TX, RX, DE/RE, address
+
+    while (true) {
+        if (encoder.update()) {
+            auto angle = encoder.get_angle();
+            auto ticks = encoder.get_position();
+            printf("AMT212C-V ticks: %u, angle: %f [rad], %f [deg]\n",
+                   ticks, angle.rad(), angle.deg());
+        } else {
+            printf("Failed to read encoder\n");
+        }
+
+        ThisThread::sleep_for(100ms);
+    }
+}
